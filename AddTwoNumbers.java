88abd8acaf9f9b8b878263be9b23c2efc5e2b930
AbhijeetRander <39645638+AbhijeetRander@users.noreply.github.com>
2018-05-30 21:56:47 +0530

    Making changes to the file

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
 public class Add
 {
   public static void main(String args[])
   {
-    int x = 10;
-    int y = 20;
+    int x = 0;
+    int y = 0;
     int z = 0;
+    Scanner scnAdd = new Scanner(System.in);
+    System.out.println("Enter the first number X: ");
+    x = scnAdd.nextInt();
+    System.out.println("Enter the second number Y: ");
+    y = scnAdd.nextInt();
     z = x + y;
     System.out.println(z);
   }
